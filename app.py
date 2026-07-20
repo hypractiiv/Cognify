95fe8d49b8282f4b195a2d507cc3566b0e3f7af0
hypractiiv <manishcse18@gmail.com>
2026-07-21 01:11:43 +0530

    Added PDF Upload Feature

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from pathlib import Path
 
 st.set_page_config(
     page_title="StudyGPT",
@@ -7,7 +8,24 @@ st.set_page_config(
 )
 
 st.title("📚 StudyGPT")
+st.subheader("AI Study Assistant")
 
-st.write("Welcome to your AI Study Assistant!")
+upload_folder = Path("data/uploads")
+upload_folder.mkdir(parents=True, exist_ok=True)
 
-st.success("Project setup completed successfully.")
+uploaded_file = st.file_uploader(
+    "Upload your study material",
+    type=["pdf"]
+)
+
+if uploaded_file is not None:
+    file_path = upload_folder / uploaded_file.name
+
+    with open(file_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+
+    st.success("PDF uploaded successfully!")
+
+    st.write("Filename:", uploaded_file.name)
+
+    st.write("Saved at:", file_path)
